f763fd2ba9f75f159fa6353947a05784a1d480ae
Kiran Joshi <kiranjoshi1649@gmail.com>
2026-04-29 07:32:36 +0530

    standardized API base url for vercel

--- billing-frontend/src/services/api.js
+++ billing-frontend/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1' ? 'http://localhost:5001/api/v1' : '/api/v1',
+  baseURL: window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1' 
+  ? 'http://localhost:5001/api' 
+  : '/api',
   headers: {
     'Content-Type': 'application/json',
   },
